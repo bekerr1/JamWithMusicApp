11a67e0b93d39c18c66a28286847621e7bad6f3d
Brendan Kerr <brendkerr5@gmail.com>
2016-03-10 14:02:50 -0500

    Prepare for Preset table view controller/ switch to metvc branch

---
+++ JWMixAudioScrubber/JWEffectPresetsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JWEffectPresetsTableViewController.h
+//  JamWDev
+//
+//  Created by brendan kerr on 3/10/16.
+//  Copyright © 2016 JOSEPH KERR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JWEffectPresetsTableViewController : UITableViewController
+
+@end
