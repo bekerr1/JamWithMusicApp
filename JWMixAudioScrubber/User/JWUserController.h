c0786b00ee3133a03aa724c7b1a49945530f8221
Brendan Kerr <brendkerr5@gmail.com>
2016-03-31 18:41:10 -0400

    added firebase framework and dependencies added user controller added user constants started the creating and posting of the user

---
+++ JWMixAudioScrubber/User/JWUserController.h
@@ -0,0 +1,14 @@
+//
+//  JWUserController.h
+//  JamWDev
+//
+//  Created by brendan kerr on 3/31/16.
+//  Copyright © 2016 JOSEPH KERR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Firebase/Firebase.h>
+
+@interface JWUserController : NSObject
+
+@end
