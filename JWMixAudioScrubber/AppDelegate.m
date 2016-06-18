7bc97552ca938920db3629635aa99a87d868d41a
JOSEPH KERR <kerrjo@me.com>
2016-06-18 15:28:32 -0400

    handle openURL for both Facebook and (airDrop and share)

--- JWMixAudioScrubber/AppDelegate.m
+++ JWMixAudioScrubber/AppDelegate.m
@@ -21,12 +21,10 @@
 
 @implementation AppDelegate
 
-
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
 
     //[[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+
     [[UINavigationBar appearance] setTitleTextAttributes:
      [NSDictionary dictionaryWithObjectsAndKeys:
       [UIColor whiteColor], NSForegroundColorAttributeName, nil]];
@@ -42,119 +40,202 @@
         [JWCurrentWorkItem sharedInstance].currentAmpImageIndex = [ampIndexNumber unsignedIntegerValue];
     
     
-    // Override point for customization after application launch.
-//    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-//    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-//    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
-//    splitViewController.delegate = self;
     return [[FBSDKApplicationDelegate sharedInstance] application:application
                                     didFinishLaunchingWithOptions:launchOptions];
 }
 
-/*
-Jan 30 01:10:55 JOes-Phone JamWDev[2111] <Warning>: -[AppDelegate application:didFinishLaunchingWithOptions:] {
-    UIApplicationLaunchOptionsAnnotationKey =     {
-        LSMoveDocumentOnOpen = 1;
-    };
-    UIApplicationLaunchOptionsURLKey = "file:///private/var/mobile/Containers/Data/Application/FE75359C-1B9E-4B09-B1C3-97E29BC1CBD4/Documents/Inbox/trimmedMP3_7710DD31-7046-497E-AA09-B30BA8AB3080.m4a";
-}
-Jan 30 01:10:55 JOes-Phone
-*/
 
-//- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
-//    
-//    NSLog(@"%s %@ %@",__func__,[url description],[options description]);
-//    
-//    BOOL moveDocOnOpen = NO;
-//    id optionsAnnotation = options[UIApplicationOpenURLOptionsAnnotationKey];
-//    if (optionsAnnotation) {
-//        id moveDocument = optionsAnnotation[@"LSMoveDocumentOnOpen"];
-//        if (moveDocument) {
-//            moveDocOnOpen = [moveDocument boolValue];
-//        }
-//    }
-//    BOOL openInPlace = NO;
-//    id optionsOpenInPlace = options[UIApplicationOpenURLOptionsOpenInPlaceKey];
-//    if (optionsOpenInPlace) {
-//        openInPlace = [optionsOpenInPlace boolValue];
-//    }
-//    BOOL isMusicMemos = NO;
-//    id optionsSourceApp = options[UIApplicationOpenURLOptionsSourceApplicationKey];
-//    if (optionsSourceApp) {
-//        NSLog(@"optionsSourceApp %@",optionsSourceApp);
-//        if ([optionsSourceApp isEqualToString:@"com.apple.musicmemos"]) {
-//            NSLog(@"MusicMemos %@",[url lastPathComponent]);
-//            isMusicMemos = YES;
-//        }
-//    }
-//
-//    
-//    NSURL *resultURL = [[JWFileController sharedInstance] processInBoxItem:url options:options];
-//
-//    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-//    UINavigationController *navigationController = [splitViewController.viewControllers firstObject];
-////    UIViewController *vc = [navigationController topViewController];
-//    MasterViewController *master = [[navigationController viewControllers] firstObject];
-//    
-////    if (resultURL && isMusicMemos) {
-////        
-////        [master performNewJamTrack:resultURL];
-////    }
-//
-//    if (resultURL) {
-//        
+#pragma mark - open url
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    
+    NSLog(@"%s %@ %@",__func__,[url description],[options description]);
+
+    BOOL moveDocOnOpen = NO;
+    BOOL openInPlace = NO;
+    BOOL isMusicMemos = NO;
+    BOOL isFaceBook = NO;
+    BOOL isSafariViewService = NO;
+
+    id optionsAnnotation = options[UIApplicationOpenURLOptionsAnnotationKey];
+    if (optionsAnnotation) {
+        id moveDocument = optionsAnnotation[@"LSMoveDocumentOnOpen"];
+        if (moveDocument) {
+            moveDocOnOpen = [moveDocument boolValue];
+        }
+    }
+    
+    id optionsOpenInPlace = options[UIApplicationOpenURLOptionsOpenInPlaceKey];
+    if (optionsOpenInPlace) {
+        openInPlace = [optionsOpenInPlace boolValue];
+    }
+
+    id optionsSourceApp = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+    if (optionsSourceApp) {
+        NSLog(@"optionsSourceApp %@",optionsSourceApp);
+        if ([optionsSourceApp isEqualToString:@"com.apple.musicmemos"]) {
+            NSLog(@"MusicMemos %@",[url lastPathComponent]);
+            isMusicMemos = YES;
+            
+        } else if ([optionsSourceApp isEqualToString:@"com.apple.SafariViewService"]) {
+            NSLog(@"SafariViewService urlscheme %@",[url scheme]);
+            isSafariViewService = YES;
+        }
+    }
+
+    
+
+    //NSString *faceBookAppScheme = [NSString stringWithFormat:@"fb%@",@"769979083137410"]; //fb769979083137410
+    //[[NSBundle mainBundle] objectForInfoDictionaryKey:@"FacebookAppID"];
+//    NSString *faceBookAppScheme =
+//    [NSString stringWithFormat:@"fb%@",
+//     [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FacebookAppID"]];
+
+    
+    NSLog(@"urlscheme %@",[url scheme]);
+
+    if ([[url scheme] isEqualToString:[NSString stringWithFormat:@"fb%@",
+                                       [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FacebookAppID"]]]) {
+        isFaceBook = YES;
+    }
+
+    // IF its facebook API handle it with FBSDKApplicationDelegate
+    if (isFaceBook) {
+        
+        id optionsAnnotation = options[UIApplicationOpenURLOptionsAnnotationKey];
+
+        return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                              openURL:url
+                                                    sourceApplication:optionsSourceApp
+                                                           annotation:optionsAnnotation];
+        // process facebook simply returns its openURL
+    }
+    
+    
+    
+    // Otherwise is a URL we handle
+    NSURL *resultURL = [[JWFileController sharedInstance] processInBoxItem:url options:options];
+
+    if (resultURL && isMusicMemos) {
+        NSLog(@"JAM With MusicMemos file %@",[url lastPathComponent]);
+
 //        [master performNewJamTrack:resultURL];
-//    } else {
-//        
-//        
-//        NSString *message;
-//        if (resultURL)
-//            message = [NSString stringWithFormat:@"Successfully Copied file %@",[url lastPathComponent]];
-//        else
-//            message = [NSString stringWithFormat:@"There was a problem copying file %@",[url lastPathComponent]];
-//        
-//        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"File Import" message:message
-//                                                                          preferredStyle:UIAlertControllerStyleAlert];
-//        
-//        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-//        }];
-//        
-//        [alertController addAction:okAction];
-//        [splitViewController presentViewController:alertController animated:YES completion:nil];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"JWAudioFileAddedToSystem" object:self
+                                                          userInfo:@{@"URL":resultURL}
+         ];
+        
+    }
+
+//    if (resultURL) {
+//        NSLog(@"JAM With filename %@",[url lastPathComponent]);
+////        [master performNewJamTrack:resultURL];
+//        [[NSNotificationCenter defaultCenter] postNotificationName:@"JWAudioFileAddedToSystem" object:self
+//                                                          userInfo:@{@"URL":resultURL}
+//         ];
 //    }
-//    
-//    return (resultURL != nil);
-//}
-//
+    
+    else {
+        
+        
+        NSString *message;
+        if (resultURL)
+            message = [NSString stringWithFormat:@"Successfully Copied file %@",[url lastPathComponent]];
+        else
+            message = [NSString stringWithFormat:@"There was a problem copying file %@",[url lastPathComponent]];
+        
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"File Import" message:message
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        }];
+        
+        [alertController addAction:okAction];
+        
+        [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
+        
+//        [splitViewController presentViewController:alertController animated:YES completion:nil];
+    }
+    
+    return (resultURL != nil);
+}
 
 
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+// OLD API deprectaed in iOS9 use openURL:(NSURL *)url options:
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
     return [[FBSDKApplicationDelegate sharedInstance] application:application
                                                                   openURL:url
                                                         sourceApplication:sourceApplication
                                                                annotation:annotation];
 }
+
+
 /*
  
  AIRDROP from mac
--[AppDelegate application:openURL:options:] file:///private/var/mobile/Containers/Data/Application/6BABD702-3B1E-42F3-8C6D-2532196EDC05/Documents/Inbox/trimmedMP3_B01D917D-5253-4D23-AA05-E39C5FB93A72.m4a {
+-[AppDelegate application:openURL:options:]
+ file:// /private/var/mobile/Containers/Data/Application/6BABD702-3B1E-42F3-8C6D-2532196EDC05/Documents/Inbox/trimmedMP3_B01D917D-5253-4D23-AA05-E39C5FB93A72.m4a {
 UIApplicationOpenURLOptionsAnnotationKey =     {
     LSMoveDocumentOnOpen = 1;
 };
 UIApplicationOpenURLOptionsOpenInPlaceKey = 0;
 }
  
- AIRDROP FROM app MusicMemos
-2016-01-29 22:16:31.445 JamWDev[2035:868086] -[AppDelegate application:openURL:options:] file:///private/var/mobile/Containers/Data/Application/6BABD702-3B1E-42F3-8C6D-2532196EDC05/Documents/Inbox/My%20Idea%207.m4a {
+ FROM app MusicMemos
+ -[AppDelegate application:openURL:options:]
+ file:// /private/var/mobile/Containers/Data/Application/6BABD702-3B1E-42F3-8C6D-2532196EDC05/Documents/Inbox/My%20Idea%207.m4a {
 UIApplicationOpenURLOptionsAnnotationKey =     {
 };
 UIApplicationOpenURLOptionsOpenInPlaceKey = 0;
 UIApplicationOpenURLOptionsSourceApplicationKey = "com.apple.musicmemos";
 }
  
+ ???? NotSure But looks like AIR Drop when app is not launched
+-[AppDelegate application:didFinishLaunchingWithOptions:] {
+ UIApplicationLaunchOptionsAnnotationKey =     {
+ LSMoveDocumentOnOpen = 1;
+ };
+ UIApplicationLaunchOptionsURLKey = "file:///private/var/mobile/Containers/Data/Application/FE75359C-1B9E-4B09-B1C3-97E29BC1CBD4/Documents/Inbox/trimmedMP3_7710DD31-7046-497E-AA09-B30BA8AB3080.m4a";
+ }
+ 
+ 
+ FROM FaceBook nonApp Safari Interface
+ [AppDelegate application:openURL:options:]
+ url	NSURL *	@"fb769979083137410://authorize/#state=%7B%22challenge%22%3A%22CgxVGEcQ1Y66OFz8kayAOZBOJPw%253D%22%2C%220_auth_logger_id%22%3A%220AE4F3E9-3558-487C-966F-DDA78EF67E2B%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&granted_scopes=email%2Ccontact_email%2Cuser_actions.music%2Cpublic_profile&denied_scopes=&signed_request=mJ-HW86txz4Jp29Amtb7VrJkVziQgSfVrOAr3JZBjd4.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUURzOFVrM1lGclA3d1h0Zm9vSjNGaTllanpnWjVrak9mVTVhVF9yZmNvcjZsSTc5U1ZBbDJ4bXlBV2tTcG5LRkpFT2lNcmxDVzFnOWMxVWFCNDB0SW40UW1DQ2l4YzRQci0zLTFMTGdWQ0R3LWNOam5NSXkwWXo4c21QTzc5c1hTekNsaU5kQjJhd1o4SWpuSGc2blF2UnMzZWFvYVRza2wtbzNYdDR2ZzBhY3ZveV9JT29Tb2M5a1BJREJXSDQ1QXZzRDhHemU2MEhPZHRXV01iQWFiTHRZckxwNmUxLVk0VmNXanBVdXhZM2RwVEI3V2VwSGdNRG1MdnJWQXc5VHlia0U1VkZjRlYzVHJweElYczZQbklMcFRZdGVjeHhNcVVsdU9udzNnS3Z1emRRbmtlT1lrbUZKdG5oWGVQNjQ5T29kaFVHWmNSckF4MUd6Z2xYNlZhTjFsWTg3bmI5NEZpODlseE9aSzNXYjZ6VkFFN3BxUll4VzRYMzNkX3hvVGsiLCJpc3N1ZWRfYXQiOjE0NjYyMjIyNzIsInVzZXJfaWQiOiIxMDIwODIwMTgx"
+ 
+ info.plist FacebookAppID 769979083137410
+ 
+ options	__NSDictionaryM *	2 key/value pairs	0x000000013a92a380
+ [0]	(null)	@"UIApplicationOpenURLOptionsSourceApplicationKey" : @"com.apple.SafariViewService"
+ key	__NSCFConstantString *	@"UIApplicationOpenURLOptionsSourceApplicationKey"	0x000000019c88d440
+ value	__NSCFString *	@"com.apple.SafariViewService"	0x0000000135df30a0
+ [1]	(null)	@"UIApplicationOpenURLOptionsOpenInPlaceKey" : @"0"
+ key	__NSCFConstantString *	@"UIApplicationOpenURLOptionsOpenInPlaceKey"	0x000000019c88d480
+ value	__NSCFBoolean *	@"0"	0x00000001a0a366d8
+ 
+ 
+ MUSIC MEMOS
+ options	__NSDictionaryM *	3 key/value pairs	0x000000015fd61970
+ [0]	(null)	@"UIApplicationOpenURLOptionsSourceApplicationKey" : @"com.apple.musicmemos"
+ [1]	(null)	@"UIApplicationOpenURLOptionsOpenInPlaceKey" : @"0"
+ [2]	(null)	@"UIApplicationOpenURLOptionsAnnotationKey" : 0 key/value pairs
+ 
+ 
+ AIRDROP
+ [AppDelegate application:openURL:options:] file:///private/var/mobile/Containers/Data/Application/328CAF3F-3A9F-41BC-92FD-FF9BA4C3BF57/Documents/Inbox/trimmedMP3_0B828B36-BE9F-4074-9538-D4E0EBA3A7D5.m4a {
+ 
+ [AppDelegate application:openURL:options:] file:///private/var/mobile/Containers/Data/Application/328CAF3F-3A9F-41BC-92FD-FF9BA4C3BF57/Documents/Inbox/trimmedMP3_0B828B36-BE9F-4074-9538-D4E0EBA3A7D5.m4a {
+ UIApplicationOpenURLOptionsAnnotationKey =     {
+ LSMoveDocumentOnOpen = 1;
+ };
+ UIApplicationOpenURLOptionsOpenInPlaceKey = 0;
+ }
+
  */
 
+#pragma mark - appl state
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -186,7 +267,33 @@ UIApplicationOpenURLOptionsSourceApplicationKey = "com.apple.musicmemos";
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+
+
+@end
+
+
 #pragma mark - Split view
+
+
+//    didFinishLaunchingWithOptions
+// Override point for customization after application launch.
+//    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+//    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+//    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
+//    splitViewController.delegate = self;
+
+
+//    - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+
+//    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+//    UINavigationController *navigationController = [splitViewController.viewControllers firstObject];
+////    UIViewController *vc = [navigationController topViewController];
+//    MasterViewController *master = [[navigationController viewControllers] firstObject];
+//
+//    if (resultURL && isMusicMemos) {
+//        [master performNewJamTrack:resultURL];
+//    }
+
 
 //- (BOOL)splitViewController:(UISplitViewController *)splitViewController collapseSecondaryViewController:(UIViewController *)secondaryViewController ontoPrimaryViewController:(UIViewController *)primaryViewController {
 //    
@@ -251,4 +358,3 @@ UIApplicationOpenURLOptionsSourceApplicationKey = "com.apple.musicmemos";
 //}
 
 
-@end
